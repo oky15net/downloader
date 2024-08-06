fae91262458804430fc89a39acd87f7496bec0ae
Okto Ramadhan <133633500+oky15net@users.noreply.github.com>
2024-08-06 11:30:28 +0700

    Update downloader_script.py
    asuk

--- scripts/downloader_script.py
+++ scripts/downloader_script.py
@@ -1,30 +1,25 @@
-# downloader_script.py
-import requests
+import modules.scripts as scripts
+import gradio as gr
 import os
 
-def download_file(url, download_folder):
-    local_filename = os.path.join(download_folder, url.split('/')[-1])
-    with requests.get(url, stream=True) as r:
-        r.raise_for_status()
-        with open(local_filename, 'wb') as f:
-            for chunk in r.iter_content(chunk_size=8192):
-                f.write(chunk)
-    return local_filename
+from modules import script_callbacks
+
 
 def on_ui_tabs():
-    with gr.Blocks() as downloader:
-        gr.Markdown("## File Downloader")
-        url_input = gr.Textbox(label="Direct Link URL")
-        download_button = gr.Button("Download")
-        output = gr.Textbox(label="Download Path")
-
-        def download_and_show(url):
-            download_folder = "downloads"
-            os.makedirs(download_folder, exist_ok=True)
-            file_path = download_file(url, download_folder)
-            return file_path
-
-        download_button.click(fn=download_and_show, inputs=url_input, outputs=output)
-    return [(downloader, "Downloader", "downloader")]
+    with gr.Blocks(analytics_enabled=False) as ui_component:
+        with gr.Row():
+            angle = gr.Slider(
+                minimum=0.0,
+                maximum=360.0,
+                step=1,
+                value=0,
+                label="hidup sullit"
+            )
+            checkbox = gr.Checkbox(
+                False,
+                label="asuk"
+            )
+            # TODO: add more UI components (cf. https://gradio.app/docs/#components)
+        return [(ui_component, "bajing loncat", "extension_template_tab")]
 
 script_callbacks.on_ui_tabs(on_ui_tabs)
